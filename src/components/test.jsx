1dd25873bbf1c1d34a7b15013b417c10a3521518
DilshanGAM <malithdilshan27@gmail.com>
2025-12-19 20:40:06 +0530

    Day 13 ✏️

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,20 +1,22 @@
 export default function Test(){
     return(
-        <div className="w-[600px] h-[600px] border-4 relative">
-			<div className="w-[500px] h-[500px] bg-amber-200 flex flex-col items-center justify-center relative">
-				<div className="w-[75px] h-[75px] bg-blue-600">
-				</div>
-				<div className="w-[75px] h-[75px] bg-red-600">
-				</div>
-				<div className="w-[75px] h-[75px] bg-green-600 absolute top-[10px] right-[10px] ">
-				</div>
-				<div className="w-[75px] h-[75px] bg-purple-600 fixed top-[10px] right-[150px] ">
-				</div>
-				<div className="w-[75px] h-[75px] bg-black">
-				</div>
-				<div className="w-[75px] h-[75px] bg-[#00FFFF]">
-				</div>
-			</div>
+        <div className="w-full h-full border-4 relative flex flex-wrap">
+			<div className="w-[200px] h-[200px] bg-red-500 mt-[20px] ml-[40px] mb-[50px] mr-[80px]  pl-[10px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, pariatur!</div>
+			<div className="w-[200px] h-[200px] bg-green-500 m-[20px]">Box 2</div>
+			<div className="w-[200px] h-[200px] bg-blue-500 ">Box 3</div>
+			<div className="w-[200px] h-[200px] bg-yellow-500 ">Box 4</div>
+			<div className="w-[200px] h-[200px] bg-red-500 ">Box 1</div>
+			<div className="w-[200px] h-[200px] bg-green-500 ">Box 2</div>
+			<div className="w-[200px] h-[200px] bg-blue-500 ">Box 3</div>
+			<div className="w-[200px] h-[200px] bg-yellow-500 ">Box 4</div>
+			<div className="w-[200px] h-[200px] bg-red-500 ">Box 1</div>
+			<div className="w-[200px] h-[200px] bg-green-500 ">Box 2</div>
+			<div className="w-[200px] h-[200px] bg-blue-500 ">Box 3</div>
+			<div className="w-[200px] h-[200px] bg-yellow-500 ">Box 4</div>
+			<div className="w-[200px] h-[200px] bg-red-500 ">Box 1</div>
+			<div className="w-[200px] h-[200px] bg-green-500 ">Box 2</div>
+			<div className="w-[200px] h-[200px] bg-blue-500 ">Box 3</div>
+			<div className="w-[200px] h-[200px] bg-yellow-500 ">Box 4</div>
 		</div>
     )
 }

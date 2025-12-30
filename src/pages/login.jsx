0c3869412ef046593ad65ea140cf6afdb59c9ed9
DilshanGAM <malithdilshan27@gmail.com>
2025-12-30 21:00:59 +0530

    day 16 ✏️

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,57 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function LoginPage() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const navigate = useNavigate()
+
+	// function login(){
+		
+	// 	axios.post("http://localhost:3000/users/login",
+	// 		{
+	// 			email : email,
+	// 			password : password
+	// 		}
+	// 	).then(
+	// 		(response)=>{
+	// 			console.log(response)
+	// 		}
+	// 	).catch(
+	// 		(error)=>{
+	// 			console.log(error)
+	// 			console.log("Login Failed")
+	// 		}
+	// 	)
+	// }
+
+	async function login(){
+		try{
+			const response = await axios.post(import.meta.env.VITE_API_URL + "/users/login",
+				{
+					email : email,
+					password : password
+				}
+			)
+			console.log(response)
+			toast.success("Login Successful")
+			if(response.data.role == "admin"){
+
+				//window.location.href = "/admin/"
+				navigate("/admin/")
+				
+			}else{
+				//redirect to home page "/"
+			}
+		}catch(error){
+			console.log(error)
+			//alert("Login Failed")
+			toast.error("Login Failed")
+		}
+	}
+
 	return (
 		<div className="w-full h-full bg-[url('/background.jpg')] bg-cover no-repeat bg-center flex">
 			<div className="w-[50%] h-full flex justify-center items-center flex-col">
@@ -12,12 +63,22 @@ export default function LoginPage() {
 					<input
 						type="email"
 						placeholder="Email"
-						className="m-5 p-3 w-[90%] h-[50px] rounded-lg border border-secondary outline-none"
+						onChange={
+							(e)=>{
+								setEmail(e.target.value)
+							}
+						}
+						className="m-5 p-3 w-[90%] h-[50px] rounded-lg border border-secondary outline-none "
 					/>
 					<input
 						type="password"
 						placeholder="Password"
 						className="m-5 p-3 w-[90%] h-[50px] rounded-lg border border-secondary outline-none"
+						onChange={
+							(e)=>{
+								setPassword(e.target.value)
+							}
+						}
 					/>
 					<p className="w-full text-right pr-5 ">
 						Forgot Password?{" "}
@@ -25,10 +86,10 @@ export default function LoginPage() {
 							Reset
 						</Link>
 					</p>
-					<button className="m-5 p-3 w-[90%] h-[50px] bg-accent rounded-lg text-white font-bold">
+					<button onClick={login} className="m-5 p-3 w-[90%] h-[50px] bg-accent rounded-lg text-white font-bold">
 						Login
 					</button>
-					<button className="m-5 p-3 w-[90%] h-[50px] border border-accent rounded-lg text-white font-bold">
+					<button  className="m-5 p-3 w-[90%] h-[50px] border border-accent rounded-lg text-white font-bold">
 						Login with Google
 					</button>
 					<p className="w-full  text-right pr-5">

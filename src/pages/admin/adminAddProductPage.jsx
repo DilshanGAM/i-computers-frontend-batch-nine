9a90129bd964ab0683518a454a715aad96b84640
DilshanGAM <malithdilshan27@gmail.com>
2026-01-09 21:07:01 +0530

    Day 18 ✏️

--- src/pages/admin/adminAddProductPage.jsx
+++ src/pages/admin/adminAddProductPage.jsx
@@ -1,13 +1,8 @@
 import { useState } from "react";
-/*
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
-    
-        isVisible : {
-            type : Boolean,
-            default : true,
-            required : true
-        },
-*/
 export default function AdminAddProductPage(){
     
     const [productId , setProductId] = useState("");
@@ -20,11 +15,49 @@ export default function AdminAddProductPage(){
     const [brand , setBrand] = useState("Standard");
     const [model , setModel] = useState("");
     const [isVisible , setIsVisible] = useState(true);
+    const navigate = useNavigate()
 
+    async function handleAddProduct(){
+        try{
+
+            const token = localStorage.getItem("token");
+
+            if(token == null){
+                toast.error("You must be logged in to add a product");
+                window.location.href = "/login";
+                return;
+            }
+            //"sound,base,audio,bluetooth"
+            //altNames.split(",") => ["sound","base","audio","bluetooth"]
+            await axios.post( import.meta.env.VITE_API_URL + "/products",{
+                productId: productId,
+                name: name,
+                description: description,
+                price : price,
+                labelledPrice: labelledPrice,
+                altNames : altNames.split(","),
+                category: category,
+                brand: brand,
+                model: model,
+                isVisible: isVisible,
+            },{
+                headers: {
+                    Authorization : "Bearer "+token
+                }
+            })
+            toast.success("Product added successfully");
+            navigate("/admin/products");
+        }catch(err){
+            // toast.error("Failed to add product");
+            toast.error(err?.response?.data?.message || "Failed to add product");
+            return;
+        }
+    }
     
     return(
-        <div className="w-full max-h-full flex flex-wrap items-start">
-            <div className="w-[50%]  h-[120px] flex flex-col">
+        <div className="w-full max-h-full flex flex-wrap items-start  overflow-y-scroll hide-scroll-track">
+            <h1 className="w-full text-3xl font-bold mb-4 sticky top-0 bg-primary">Add New Product</h1>
+            <div className="w-[50%]   h-[120px] flex flex-col">
                 <label className="font-bold ml-2">Product ID</label>
                 <input value={productId} onChange={(e)=>{setProductId(e.target.value)}}  placeholder="Ex: ID001" className="border-4 border-accent rounded-[10px] h-[50px] p-2 m-2 focus:outline-white"/>
             </div>
@@ -77,10 +110,14 @@ export default function AdminAddProductPage(){
             </div>
             <div className="w-[25%]  h-[120px] flex flex-col">
                 <label className="font-bold ml-2">Is Visible</label>
-                <select value={isVisible} onChange={(e)=>{setIsVisible(e.target.value)}} className="border-4 border-accent rounded-[10px] h-[50px] p-2 m-2 focus:outline-white">
+                 <select value={isVisible} onChange={(e)=>{setIsVisible(e.target.value)}} className="border-4 border-accent rounded-[10px] h-[50px] p-2 m-2 focus:outline-white">
                     <option value={true}>Yes</option>
                     <option value={false}>No</option>
                 </select>
+            </div>
+            <div className="w-full h-[80px] bg-white sticky bottom-0 rounded-b-2xl flex justify-end items-center p-4 gap-4 ">
+                <button className="bg-gray-400 text-white font-bold px-6 py-3 rounded-[10px] hover:bg-gray-500">Cancel</button>
+                <button onClick={handleAddProduct} className="bg-accent text-white font-bold px-6 py-3 rounded-[10px] hover:bg-secondary">Add Product</button>
             </div>
         </div>
     )

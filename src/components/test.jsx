18c341009987540d40fc01bdeed4e586073d1d8d
DilshanGAM <malithdilshan27@gmail.com>
2026-02-06 21:06:17 +0530

    Day 23 ✏️

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react"
+
 export default function Test(){
+	
+	const [number , setNumber] = useState(11)
+
 	return(
 		<div className="w-full h-full flex justify-center items-center">
-			
-			
+			<h1>{number}</h1>
+			<button onClick={()=>{
 
-		</div>
+				setNumber(number + 1)
+
+			}}>Increase</button>
+		</div>	
 	)
 }
